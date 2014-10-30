f3cac1555062f00afdf1aa5433b4506f632c2807
tev <tvaillard@gmail.com>
2014-10-30 22:00:47 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+#TIVI SAMPLE APP 
+[*Ruby on Rails Tutorial: Learn Rails by Example*](http://railstutorial.org/)
+by [Michael Hartl](http://michaelhartl.com/).
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'a96567722190e1bc89c591a9adee5e87eac1a5721a048812042d469e7f8c265db56a77884d196946b933d7483fff9e3ea8a49659d895405f1b7d4c3ffae5dea2'
