e0fac1edb9c465347ed344a237e5c217e0a125ea
tev <tvaillard@gmail.com>
2014-11-30 13:52:27 +0100

    Correction image

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,7 +4,7 @@ module UsersHelper
    def gravatar_for(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     #gravatar_url = "https://secure.gravatar.com/avatars/#{gravatar_id}.png"
-    gravatar_url = "tv.jpg"    
+    gravatar_url = "assets/tv.jpg"    
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
    end
 end
