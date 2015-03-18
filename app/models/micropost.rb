3dd0a6c5c29f51684faec7f2cb960bdc497ef329
tev <tvaillard@gmail.com>
2015-03-18 14:07:32 +0100

    Fin chap 11

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,4 +5,18 @@ class Micropost < ActiveRecord::Base
   validates :content, presence: true, length: {maximum: 140} 
   # par defaut extraction en ordre decroissant de created_at
   default_scope order: 'microposts.created_at DESC'
+
+  def self.from_users_followed_by(user)
+  # Convient pour un nombre reduit de followeds
+  # followed_user_ids = user.followed_user_ids
+  # where("user_id IN (?) OR user_id = ?", followed_user_ids, user)
+
+  # Sinon utilisation de SQL subselect :
+
+  followed_user_ids = "SELECT followed_id FROM relationships
+                 WHERE follower_id = :user_id"
+  where("user_id IN (#{followed_user_ids}) OR user_id = :user_id",
+  user_id: user.id)
+  end
+
 end
